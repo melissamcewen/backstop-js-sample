b763874ad57757bac10aa2ebbe7f8fa7890f29cd
Melissa Mcewen <melissa.mcewen@rga.com>
2016-11-30 16:51:48 -0600

    base config of backstop for localhost based site and some casper actions for age gate passing

--- backstop_data/casper_scripts/onBefore.js
+++ backstop_data/casper_scripts/onBefore.js
@@ -1,16 +1,13 @@
-module.exports = function (casper, scenario, vp) {
-  //This script runs before your app loads. Edit here to log-in, load cookies or set other states required for your test.
-  console.log('onBefore.js has run for '+ vp.name + '.');
-};
+module.exports = function(casper, scenario, vp) {
 
-// // EXAMPLE: LOGIN BEFORE RUNNING TESTS
-// module.exports = function(casper, scenario, vp) {
-//   casper.thenOpen(scenario.url, function(){
-//      if (this.exists('form#user-login-form')) {
-//        this.fill('form#loginForm',{
-//           'username': 'test',
-//           'password': 'changeme'
-//        }, true);
-//      }
-//   });
-// };
+//this site has an age gate (it's for a booze brand, so let's get past it)    
+  casper.thenOpen(scenario.url, function(){
+     if (this.exists('form.age-gate__form')) {
+       this.fill('form.age-gate__form',{
+          'month': '06',
+          'day': '06',
+          'year': '1986'
+       }, true);
+     }
+  });
+};
